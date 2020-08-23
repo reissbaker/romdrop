7cf1de1817c644e24ddff37a6cd09b8af1df52c1
Matt Baker <matthew.reiss.baker@gmail.com>
2020-08-23 05:53:16 -0700

    Support file upload

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use handlebars::Handlebars;
-use actix_web::{web, get, App, HttpResponse, HttpServer, Result};
+use std::{str, env};
+use actix_http::error::ErrorUnprocessableEntity;
+use actix_multipart::Multipart;
+use actix_web::{
+    web,
+    http,
+    middleware,
+    get,
+    post,
+    App,
+    HttpResponse,
+    HttpServer,
+    Result,
+};
 use actix_web::error::{ErrorInternalServerError, ErrorNotFound};
 use actix_files::Files;
+use handlebars::Handlebars;
+use tokio::stream::StreamExt;
+use tokio::prelude::*;
 use serde::{Serialize, Deserialize};
-use std::{str, env};
 
 #[derive(Serialize, Clone)]
 struct EmulatorData<'a> {
@@ -31,6 +45,8 @@ const EMULATORS: &'static [EmulatorData<'static>] = &[
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     let port = match env::args().nth(1) {
         Some(port) => port,
         None => {
@@ -38,11 +54,15 @@ async fn main() -> std::io::Result<()> {
             std::process::exit(1);
         }
     };
+
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Compress::default())
+            .wrap(middleware::Logger::default())
             .service(Files::new("/static", "assets/static"))
-            .service(index)
-            .service(emulator)
+            .service(index_page)
+            .service(emulator_page)
+            .service(upload_rom)
     })
     .bind(format!("127.0.0.1:{}", port))?
     .run()
@@ -55,7 +75,7 @@ struct IndexTemplateData {
 }
 
 #[get("/")]
-async fn index() -> Result<HttpResponse> {
+async fn index_page() -> Result<HttpResponse> {
     let reg = Handlebars::new();
     let index = read_file("assets/pages/index.html").await?;
     let data = IndexTemplateData {
@@ -63,39 +83,91 @@ async fn index() -> Result<HttpResponse> {
     };
     let rendered = reg.render_template(&index, &data)
         .map_err(ErrorInternalServerError)?;
-    Ok(HttpResponse::Ok().body(rendered))
+    Ok(html_response(rendered))
 }
 
 #[derive(Serialize)]
 struct UploadTemplateData<'a> {
     emulator: &'a str,
+    slug: &'a str,
 }
 #[derive(Deserialize)]
 struct EmulatorPath {
     name: String,
 }
 #[get("/system/{name}")]
-async fn emulator(emulator_path: web::Path<EmulatorPath>) -> Result<HttpResponse> {
-    let slug = emulator_path.name.clone();
-    let matching_slug = EMULATORS.iter().find(|emulator_data| {
+async fn emulator_page(emulator_path: web::Path<EmulatorPath>) -> Result<HttpResponse> {
+    let emulator = parse_emulator(&emulator_path.name)?;
+    let reg = Handlebars::new();
+    let upload = read_file("assets/pages/upload.html").await?;
+    let data = UploadTemplateData {
+        emulator: &emulator.heading.to_uppercase(),
+        slug: &emulator.slug,
+    };
+    let rendered = reg.render_template(&upload, &data)
+        .map_err(ErrorInternalServerError)?;
+    Ok(html_response(rendered))
+}
+
+
+#[post("/rom/{name}")]
+async fn upload_rom(
+    emulator_path: web::Path<EmulatorPath>,
+    mut payload: Multipart,
+) -> Result<HttpResponse> {
+    let emulator = parse_emulator(&emulator_path.name)?;
+    println!("hi");
+
+    // Parse the filename
+    let mut field = payload
+        .try_next()
+        .await?
+        .ok_or_else(|| ErrorUnprocessableEntity("No files sent"))?;
+    println!("hi");
+    let content_type = field
+        .content_disposition()
+        .ok_or_else(|| actix_web::error::ParseError::Incomplete)?;
+    println!("contenty");
+    let filename = content_type
+        .get_filename()
+        .ok_or_else(|| actix_web::error::ParseError::Incomplete)?;
+    println!("fil");
+    let path = sanitize_filename::sanitize(&filename);
+
+    // Create the appropriate emulator dir if necessary
+    let dir = format!("data/roms/{}", emulator.slug);
+    tokio::fs::create_dir_all(dir.clone()).await?;
+
+    // Write all the bytes of the stream to disk
+    let mut file = tokio::fs::File::create(
+        format!("{}/{}", dir, path)
+    ).await?;
+    while let Some(chunk) = field.next().await {
+        let data = chunk?;
+        file.write_all(&data).await?
+    }
+
+    Ok(HttpResponse::Created().finish())
+}
+
+/// Given a slug, returns the matching EmulatorData.
+///
+fn parse_emulator(slug: &str) -> Result<&EmulatorData<'static>> {
+    let emulator_search = EMULATORS.iter().find(|emulator_data| {
         emulator_data.slug == slug
     });
-    match matching_slug {
-        None => Err(ErrorNotFound("not found")),
-        Some(matching_slug) => {
-            let reg = Handlebars::new();
-            let upload = read_file("assets/pages/upload.html").await?;
-            let data = UploadTemplateData {
-                emulator: &matching_slug.heading.to_uppercase(),
-            };
-            let rendered = reg.render_template(&upload, &data)
-                .map_err(ErrorInternalServerError)?;
-            Ok(HttpResponse::Ok().body(rendered))
-        }
+    match emulator_search {
+        None => Err(ErrorNotFound(format!("No emulator: {}", slug))),
+        Some(emulator_data) => Ok(emulator_data)
     }
 }
 
 async fn read_file(path: &str) -> Result<String> {
     let bytes = tokio::fs::read(path).await?;
     Ok(str::from_utf8(&bytes)?.to_string())
+}
+
+fn html_response(body: String) -> HttpResponse {
+    HttpResponse::Ok().set_header(http::header::CONTENT_TYPE, "text/html; charset=utf-8")
+        .body(body)
 }
